8bd114b5938ec56770b95dd76ac0f5f62909f8ca
Eduardo Andrade <edu2andrade@gmail.com>
2022-07-13 14:17:34 +0100

    Home finished --> loading posts whith pagination (next_page)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import Link from 'next/link';
 import { FiCalendar, FiUser } from 'react-icons/fi';
 import { format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
+
+import { PrismicDocument } from '@prismicio/types';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -30,15 +33,39 @@ interface HomeProps {
 }
 
 export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const [posts, setPosts] = useState<Post[]>(postsPagination.results);
+  const [nextPage, setNextPage] = useState<string>(postsPagination.next_page);
+
+  async function handleMorePosts(): Promise<void> {
+    const response = await fetch(nextPage);
+    const jsonResponse = await response.json();
+
+    const morePosts = jsonResponse.results.map((post: PrismicDocument) => ({
+      uid: post.uid,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd-MMM-yyyy',
+        { locale: ptBR }
+      ),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    }));
+
+    setPosts([...posts, ...morePosts]);
+    setNextPage(jsonResponse.next_page);
+  }
+
   return (
     <>
       <header className={styles.headerContainer}>
         <img src="/spacetraveling.svg" alt="logo" />
-        <span>1</span>
       </header>
       <main className={styles.mainContainer}>
         <div className={styles.contentContainer}>
-          {postsPagination.results.map(post => (
+          {posts.map(post => (
             <Link key={post.uid} href={`/post/${post.uid}`}>
               <a title={`Post: ${post.data.title}`}>
                 <strong>{post.data.title}</strong>
@@ -56,6 +83,13 @@ export default function Home({ postsPagination }: HomeProps): JSX.Element {
               </a>
             </Link>
           ))}
+          <button
+            type="button"
+            onClick={handleMorePosts}
+            className={styles.loadPosts}
+          >
+            Carregar mais posts
+          </button>
         </div>
       </main>
     </>
@@ -66,6 +100,7 @@ export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient({});
   const postsResponse = await prismic.getByType('posts', {
     fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+    pageSize: 4,
   });
 
   const response = postsResponse.results.map(post => {
